7d86df886d3cd5051443befdc720b15dd2ac8dfc
SCRanaweera <ranaweerasc65@gmail.com>
2024-05-09 00:03:53 +0530

    Home Page Changes 1.0

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -7,22 +7,210 @@ import {
   ListItem,
   OrderedList,
   Tag,
-  Text,
+  Text,IconProps,
+  useColorModeValue,
+  Flex,Container,Stack,Button,Icon,IconButton,createIcon,Box,Image,
 } from '@chakra-ui/react'
 import React from 'react'
 import { Layout } from '../components/Layout'
 import { Link } from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext'
+import { useNavigate } from 'react-router-dom';
+
+import heroImage from '../images/heroimage.jpg';
+import chatbotImage from '../images/chatbot.jpg';
 
 export default function Homepage() {
   const {currentUser}= useAuth()
+  const navigate = useNavigate()
+
   return (
     <Layout>
       
-      <Heading>Home page</Heading>
+      <Container maxW={'5xl'}>
+      <Stack
+        textAlign={'center'}
+        align={'center'}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 20, md: 28 }}>
+
+        <Heading
+          fontWeight={600}
+          fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
+          lineHeight={'80%'}>
+          Free Online{' '}
+        <Text as={'span'} color={'red.500'} >
+          AI Image Generator
+        </Text>
+        <Box>
+        <Text as={'span'} color={'blue.400'} display="block" fontSize="lg"fontWeight="bold">
+        Welcome to Text2Pic, where imagination meets Education! 
+        </Text>
+        </Box>
+        </Heading>
+
+        <Text color={'gray.500'} maxW={'3xl'}fontSize="lg">
+        Watch words come to life in vibrant illustrations, making English learning a delightful adventure!
+        </Text>
+
+        
+      </Stack>
+    </Container>
 
 
 
+<Container maxW={'7xl'}>
+  <Stack>
+      <Flex
+        flex={1}
+        justify={'center'}
+        align={'center'}
+        position={'relative'}
+        w={'full'}>
+         
+      <Box
+        position={'relative'}
+        height={'400px'}
+        rounded={'2xl'}
+        boxShadow={'2xl'}
+        width={'600px'}
+        overflow={'hidden'}>
+
+            <Image
+              alt={'Hero Image'}
+              fit={'cover'}
+              align={'center'}
+              w={'100%'}
+              h={'100%'}
+              src={heroImage}
+            />
+            <Image/>
+          </Box>
+        </Flex>
+        </Stack>
+    </Container>
+
+    <Container maxW={'7xl'}>
+      <Stack
+        align={'center'}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 20, md: 28 }}
+        direction={{ base: 'column', md: 'row' }}>
+        <Stack flex={1} spacing={{ base: 5, md: 10 }}>
+          <Heading
+            lineHeight={1.1}
+            fontWeight={600}
+            fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}>
+            <Text
+              as={'span'}
+              position={'relative'}
+              _after={{
+                content: "''",
+                width: 'full',
+                height: '30%',
+                position: 'absolute',
+                bottom: 1,
+                left: 0,
+                bg: 'blue.400',
+                zIndex: -1,
+              }}>
+              Interactive Chatbot
+            </Text>
+            <br />
+            <Text as={'span'} color={'blue.400'}>
+            Making communication effortless!
+            </Text>
+          </Heading>
+          <Text color={'gray.500'} fontSize="lg">
+          Text2Pic is an innovative tool designed to enhance children's English learning experience.
+          Text2Pic allows children to create their own image-based vocabulary, categorize them, and access them anytime, anywhere. 
+          Best of all, it's completely free! 
+          Start exploring the world of language through captivating images with Text2Pic today!
+          </Text>
+
+
+
+          <Stack
+            spacing={{ base: 4, sm: 6 }}
+            direction={{ base: 'column', sm: 'row' }}>
+            <Button
+              rounded={'full'}
+              size={'lg'}
+              fontWeight={'normal'}
+              px={6}
+              colorScheme={'red'}
+              bg={'pink.400'}
+              _hover={{ bg: 'pink.500' }}
+              onClick={() => navigate('/login')}>
+              Get Started
+            </Button>
+            
+          </Stack>
+        </Stack>
+        <Flex
+          flex={1}
+          justify={'center'}
+          align={'center'}
+          position={'relative'}
+          w={'full'}>
+          <Blob
+            w={'150%'}
+            h={'150%'}
+            position={'absolute'}
+            top={'-20%'}
+            left={0}
+            zIndex={-1}
+            color={useColorModeValue('blue.50', 'blue.400')}
+          />
+          <Box
+            position={'relative'}
+            height={'300px'}
+            rounded={'2xl'}
+            boxShadow={'2xl'}
+            width={'full'}
+            overflow={'hidden'}>
+
+            <Image
+              alt={'Hero Image'}
+              fit={'cover'}
+              align={'center'}
+              w={'100%'}
+              h={'100%'}
+              src={chatbotImage}
+            />
+            <Image/>
+          </Box>
+        </Flex>
+      </Stack>
+    </Container>
+
+
+
+    
     </Layout>
   )
 }
+
+
+
+export const Blob = (props) => {
+  return (
+    <Icon
+      width={'100%'}
+      viewBox="0 0 578 440"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M239.184 439.443c-55.13-5.419-110.241-21.365-151.074-58.767C42.307 338.722-7.478 282.729.938 221.217c8.433-61.644 78.896-91.048 126.871-130.712 34.337-28.388 70.198-51.348 112.004-66.78C282.34 8.024 325.382-3.369 370.518.904c54.019 5.115 112.774 10.886 150.881 49.482 39.916 40.427 49.421 100.753 53.385 157.402 4.13 59.015 11.255 128.44-30.444 170.44-41.383 41.683-111.6 19.106-169.213 30.663-46.68 9.364-88.56 35.21-135.943 30.551z"
+        fill="currentColor"
+      />
+    </Icon>
+  );
+};
+
+
+
+
