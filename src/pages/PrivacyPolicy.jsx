cc046a6c91ac36c9d27681ab1198a85152078ee5
SCRanaweera <ranaweerasc65@gmail.com>
2024-05-09 22:48:13 +0530

    Privacy Policy and TOS

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -1,9 +1,9 @@
 import {
     Badge,
     chakra,
-    Code,
+    Stack,
     Heading,
-    List,
+    Container,
     ListItem,
     OrderedList,
     Tag,
@@ -17,9 +17,132 @@ import {
   export default function PrivacyPolicyPage() {
     const {currentUser}= useAuth()
     return (
-      <Layout>
-        
-        <Heading>Privacy Policy</Heading>
+    <Layout>
+        <Container maxW={'7xl'}>
+
+          <Stack align={'center'} justify={'center'}>
+            <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: '3xl', sm: '4xl', lg: '5xl' }}>
+              <Text as={'span'} color={'red.400'}>
+                Terms and Conditions
+              </Text>  
+            </Heading>           
+          </Stack>
+
+          <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: 'xl', sm: '2xl', lg: '3xl' }}>
+              <br/>           
+              <Text as={'span'}>
+              General info:
+              </Text>
+          </Heading>
+
+          <Text textAlign={'justify'}>
+          Your privacy is important to us. It is Text2Pic's policy to respect your privacy and comply with any applicable 
+          law and regulation regarding any personal information we may collect about you, including across our websites and other sites 
+          we own and operate. Personal information is any information about you that can be used to identify you. This includes information about you as a person 
+          (such as name, address, and date of birth), your devices, payment details, and even information about how you use a website or online 
+          service.
+
+          In the event our site contains links to third-party sites and services, please be aware that those sites and services have 
+          their own privacy policies. After following a link to any third-party content, you should read their posted privacy policy 
+          information about how they collect and use personal information. This Privacy Policy does not apply to any of your activities 
+          after you leave our site.
+
+          <br/>
+          <br/>
+          This policy is effective as of 09 May 2024. Last updated: 09 May 2024.
+          </Text>
+
+          <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: 'xl', sm: '2xl', lg: '3xl' }}>
+              <br/>           
+              <Text as={'span'}>
+              Amendments:
+              </Text>
+          </Heading>
+
+          <Text textAlign={'justify'}>
+          We may, at any time and at our discretion, vary this Privacy Policy by publishing the amended Privacy Policy on our website. 
+           recommend you check our website regularly to ensure you are aware of our current Privacy Policy. In the event we make any significant 
+           changes to this Privacy Policy we will use our reasonable endeavours to inform you of such changes in advance in writing.
+
+          It is important that the personal information we hold about you is true, complete, accurate and current. Accordingly, you must 
+          notify us of any changes to your personal information (for example, if you change your email address).
+          </Text>
+
+          <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: 'xl', sm: '2xl', lg: '3xl' }}>
+              <br/>           
+              <Text as={'span'}>
+              The information we collect:
+              </Text>
+          </Heading>
+
+          <Text textAlign={'justify'} >
+          We collect various types of personal information about you, 
+          including your name, contact details, preferences, payment information, and usage data when you use our software
+           or visit our website. This may include browser session data, geo-location, and device information.
+          We do not actively request sensitive information unless required by law. 
+          If collected inadvertently, we handle it with your consent and in accordance with legal requirements. Sensitive information may include racial 
+          or ethnic origin, political opinions, religion, health information, etc.</Text>
+
+          <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: 'xl', sm: '2xl', lg: '3xl' }}>
+              <br/>           
+              <Text as={'span'}>
+              Registration profile:
+              </Text>
+          </Heading>
+
+          <Text textAlign={'justify'} >
+          If you decide to create an account on the Website, you may be required to provide Us
+           your Personal Data such as e-mail address, country and encrypted password with the submission of the registration form. You may also include additional Personal Data such as your username.</Text>
+
+          <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: 'xl', sm: '2xl', lg: '3xl' }}>
+              <br/>           
+              <Text as={'span'}>
+              Security:
+              </Text>
+          </Heading>
+
+          <Text textAlign={'justify'} >
+          We treat the security for all information submitted or provided by you to Us seriously and is of importance to Us. We will take reasonable steps to protect or secure this data, including your Personal Data. Accordingly, 
+          We may require Our employees or third-party service providers to assist Us in implementing or applying 
+          appropriate securities measures (such as firewalls, penetration testing and etc). You are also advised not to disclose and secure your own username and password.
+          </Text>
+
+          <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: 'xl', sm: '2xl', lg: '3xl' }}>
+              <br/>           
+              <Text as={'span'}>
+              Changes to this Privacy Policy:
+              </Text>
+          </Heading>
+
+          <Text textAlign={'justify'} >
+          We will keep this Privacy Policy under regular review, and We may update this Privacy Policy from time to time. The updated version shall apply and supersede all previous versions. Hence, it is advisable that you check this Privacy Policy on a 
+          regular basis.</Text>
+
+          
+
+          </Container>
+
   
   
   
--- src/pages/TermsCondition.jsx
+++ src/pages/TermsCondition.jsx
@@ -1,9 +1,9 @@
 import {
-    Badge,
-    chakra,
-    Code,
+    Container,
+    Flex,
+    Box,
     Heading,
-    List,
+    Stack,
     ListItem,
     OrderedList,
     Tag,
@@ -18,8 +18,198 @@ import {
     const {currentUser}= useAuth()
     return (
       <Layout>
-        
-        <Heading>Terms and Conditions</Heading>
+        <Container maxW={'7xl'}>
+
+          <Stack align={'center'} justify={'center'}>
+            <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: '3xl', sm: '4xl', lg: '5xl' }}>
+
+              <Text as={'span'} color={'red.400'}>
+                Terms{" "}
+              </Text>
+
+              <Text
+                as={'span'}
+                position={'relative'}
+                >
+                and  
+              </Text>
+
+              <Text as={'span'} color={'red.400'}>
+                {" "}Conditions
+              </Text>
+            </Heading>
+           
+          </Stack>
+
+          <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: 'xl', sm: '2xl', lg: '3xl' }}>
+              <br/>           
+              <Text as={'span'}>
+              Acceptance of terms:
+              </Text>
+          </Heading>
+
+          <Text textAlign={'justify'}>
+          By accessing or using Text2Pic, 
+          you agree to be bound by these terms of service (the "Terms") 
+          and our privacy policy (the "Privacy Policy"). 
+          If you do not agree to these Terms or the Privacy Policy, 
+          do not use the platform.
+          </Text>
+
+          <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: 'xl', sm: '2xl', lg: '3xl' }}>
+              <br/>           
+              <Text as={'span'}>
+              Accounts:
+              </Text>
+          </Heading>
+
+          <Text textAlign={'justify'}>
+          In order to use certain features of Text2Pic, 
+          you must register for an account 
+          and provide certain information about yourself 
+          as prompted by the account registration form. 
+          You represent and warrant that: 
+          (a) all required registration information you submit 
+          is truthful and accurate; and (b) 
+          you will maintain the accuracy of such information. 
+          You may delete your Account at any time, 
+          for any reason, by following the instructions on Text2Pic. 
+          </Text>
+
+          <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: 'xl', sm: '2xl', lg: '3xl' }}>
+              <br/>           
+              <Text as={'span'}>
+              Modifications:
+              </Text>
+          </Heading>
+
+          <Text textAlign={'justify'} >
+          Company reserves the right, at any time, to modify, suspend, or discontinue Text2Pic (in whole or in part) 
+          with or without notice to you. You agree that Company will not be liable to you or to any third party for 
+          any modification, suspension, or discontinuation of Text2Pic or any part thereof.
+          </Text>
+
+          <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: 'xl', sm: '2xl', lg: '3xl' }}>
+              <br/>           
+              <Text as={'span'}>
+              Data privacy:
+              </Text>
+          </Heading>
+
+          <Text textAlign={'justify'} >
+          Any data that you input into the platform, including but not limited to personal data, will be treated as 
+          confidential in accordance with the terms of the Privacy Policy. The platform may use this data to provide 
+          analytics and insights, but will not share this data with third parties without your consent, except as 
+          required by law or as necessary to provide the platform's services.
+          </Text>
+
+          <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: 'xl', sm: '2xl', lg: '3xl' }}>
+              <br/>           
+              <Text as={'span'}>
+              Termination:
+              </Text>
+          </Heading>
+
+          <Text textAlign={'justify'} >
+          Text2Pic reserves the right to terminate or suspend your access to the platform at any time, 
+          for any reason, and without notice.
+          </Text>
+
+          <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: 'xl', sm: '2xl', lg: '3xl' }}>
+              <br/>           
+              <Text as={'span'}>
+              Governing law:
+              </Text>
+          </Heading>
+
+          <Text textAlign={'justify'} >
+          These Terms and your use of the platform will be governed by and construed in accordance with the laws of 
+          Sri Lanka, without giving effect to any principles of conflicts of law.
+          </Text>
+
+          <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: 'xl', sm: '2xl', lg: '3xl' }}>
+              <br/>           
+              <Text as={'span'}>
+              Disclaimers:
+              </Text>
+          </Heading>
+
+          <Text textAlign={'justify'}>
+          Text2Pic is provided "as is" and without warranties of any kind, either express or implied. 
+          Text2Pic does not guarantee that the platform will be error-free or uninterrupted. 
+          Text2Pic will not be liable for any damages resulting from the use of the platform, including but not limited 
+          to direct, indirect, incidental, consequential, or punitive damages.
+          Text2Pic may change at any time and in any manner, at the Company’s sole discretion. 
+          This includes, without limitation, updates to and discontinuation of features previously available. 
+          We make no guarantees related to the quality, reliability or uptime of Text2Pic. 
+          We make no representations or warranties related to current law that might apply to you. 
+          We advise you to refrain from relying on Text2Pic or creating any dependency related 
+          to Text2Pic in any manner. You acknowledge and agree that (a) we will have no liability to you, 
+          your customers or any third party for any harm caused by your reliance or dependency on Text2Pic and 
+          (b) you are solely responsible for determining the legality of your use and redistribution of Text2Pic Content. 
+          </Text>
+
+          <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: 'xl', sm: '2xl', lg: '3xl' }}>
+              <br/>           
+              <Text as={'span'}>
+              Copyright/Trademark Information:
+              </Text>
+          </Heading>
+
+          <Text textAlign={'justify'}>
+          Copyright 2024 design and develop by Text2Pic. All rights are reserved. 
+          All trademarks, logos and service marks (“Marks”) displayed on Text2Pic are our property or the 
+          property of other third parties. You are not permitted to use these Marks without our prior written consent 
+          or the consent of such third party which may own the Marks. 
+          </Text>
+
+          <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: 'xl', sm: '2xl', lg: '3xl' }}>
+              <br/>           
+              <Text as={'span'}>
+              Changes to terms of service:
+              </Text>
+          </Heading>
+
+          <Text textAlign={'justify'} >
+          Text2Pic reserves the right to update these Terms at any time and will post any updates on this page. 
+          It is your responsibility to review these Terms periodically for any changes. 
+          Your continued use of the platform after any changes have been made will constitute acceptance of those changes.
+          </Text>
+
+
+
+          </Container>
+
   
   
   
